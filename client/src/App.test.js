913cea805d65fea00cf88c111acb9b0d351dd4bd
Dave Kane <davelkane@gmail.com>
2019-09-16 22:05:03 -0500

    Added new test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+import { render, waitForElement } from '@testing-library/react'
 import App from './App';
 
 test("renders without crashing", () => {
@@ -8,6 +8,14 @@ test("renders without crashing", () => {
 
 test("contains player cards", () => {
   const { getByTestId } = render(<App />)
-
+  
   getByTestId(/player-cards/i)
 })
+
+test("App contains Megan Rapinoe", async () => {
+  const wrapper = render(<App />);
+  const player = await waitForElement(() =>
+    wrapper.queryByText(/megan rapinoe/i)
+  );
+  expect(player).toBeTruthy();
+});
